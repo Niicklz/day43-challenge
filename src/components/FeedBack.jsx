df6eb806cf7f41aaf8db944aa90acd9fc05500ad
Niicklz <77895883+Niicklz@users.noreply.github.com>
2023-10-11 14:31:55 -0400

    fix

--- src/components/FeedBack.jsx
+++ src/components/FeedBack.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./FeedBack.css";
+import Neutral from "/public/Neutral.png"
+import Satisfied from "/public/Satisfied.png"
+import unHappy from "/public/UnHappy.png"
 
 export const FeedBack = ({
     img,
@@ -9,7 +12,7 @@ export const FeedBack = ({
 }) => {
   return (
     <div className={`feedback-container ${currentFeedBackSelected === type? "active": ""} `}onClick={()=> handleChangeFocus(type)}>
-      <img src={`/public/${type}.png`} alt="" className="facetype" />
+      <img src={img} alt="" className="facetype" />
       <p className="type">{type}</p>
     </div>
   );
